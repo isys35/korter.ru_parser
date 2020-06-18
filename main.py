1920a4148a020dd074fcb6117966ab9271172705
isys35 <isys35@mail.ru>
2020-06-18 16:52:04 +0300

    обновление инфы

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import os
 from parsing_base import Parser
 from typing import NamedTuple
 
+HOST = 'https://korter.ru'
 
 def save_file(txt: str, file_name: str):
     with open(file_name, 'w', encoding='utf8') as file:
@@ -172,60 +173,42 @@ class NewBuildingsData(Parser):
 class BuildingsParser(Parser):
     def __init__(self):
         super().__init__()
-        self.host = 'https://korter.ru'
         self.cities = []
 
     def get_cities_urls(self):
-        resp = self.request.get(self.host)
+        resp = self.request.get(HOST)
         soup = BeautifulSoup(resp.text, 'lxml')
-        city_urls = [self.host + quote(city_block['href']) for city_block in soup.select('.SeoLink__StyledWrapper-sc-7zimy-0')
+        city_urls = [HOST + quote(city_block['href']) for city_block in soup.select('.SeoLink__StyledWrapper-sc-7zimy-0')
                      if 'новостройки' in city_block['href']]
         return city_urls
 
-    def update_cities(self):
+    def start(self):
         self.cities = self.load_object('cities')
         if not self.cities:
             cities_urls = self.get_cities_urls()
             self.cities = [City(url) for url in cities_urls]
-            self.save_object(self.cities, 'cities')
-        cities_for_update = [city for city in self.cities if not city.name or not city.newbuildings]
-        resps = self.requests.get([city.url for city in cities_for_update])
-        for index_resps in range(len(resps)):
-            cities_for_update[index_resps].html_code = resps[index_resps]
-        for city in cities_for_update:
-            print('Обновление имени города...')
+            resps = self.requests.get([city.url for city in self.cities])
+            for index_resps in range(len(resps)):
+                self.cities[index_resps].html_code = resps[index_resps]
+        for city in self.cities:
             city.update_name()
-            print(city.name)
         self.save_object(self.cities, 'cities')
-        for city in cities_for_update:
+        for city in self.cities:
             city.update_all_pages()
         self.save_object(self.cities, 'cities')
-
-    def get_newbuildings_urls(self, url_city_list):
-        print('[INFO] Получение ссылок на новостройки в городах')
-        start_time = time.time()
-        resps = self.requests.get(url_city_list)
-        newbuildings_city = {}
-        for index_resp in range(len(resps)):
-            city = unquote(url_city_list[index_resp]).split('/')[-1]
-            max_page = self.get_max_page(resps[index_resp])
-            pages_url = [url_city_list[index_resp]]
-            page = 1
-            if max_page:
-                while page != max_page:
-                    while page != max_page:
-                        page += 1
-                        page_url = url_city_list[index_resp] + f'?page={page}'
-                        pages_url.append(page_url)
-                    resp_page = self.request.get(pages_url[-1])
-                    max_page = self.get_max_page(resp_page.text)
-            resps_all_pages = self.requests.get(pages_url)
-            newbuildings_urls = []
-            for resp in resps_all_pages:
-                newbuildings_urls.extend(self.parsing_newbuildings_urls(resp))
-            newbuildings_city[city] = newbuildings_urls
-        print('{} секунд'.format(time.time()-start_time))
-        return newbuildings_city
+        for city in self.cities:
+            for page in city.pages_objects:
+                page.update_newbuildings()
+        self.save_object(self.cities, 'cities')
+        for city in self.cities:
+            city.update_newbuildings()
+        self.save_object(self.cities, 'cities')
+        for city in self.cities:
+            if city.name and city.newbuildings:
+                city.html_code = str()
+                for page in city.pages_objects:
+                    page.html_code = str()
+        self.save_object(self.cities, 'cities')
 
 
 class City(Parser):
@@ -264,8 +247,11 @@ class City(Parser):
         for index_resps in range(len(resps_all_pages)):
             self.pages_objects[index_resps].html_code = resps_all_pages[index_resps]
 
-    def clear_html_code(self):
-        self.html_code = str()
+    def update_newbuildings(self):
+        if self.newbuildings:
+            return
+        for page in self.pages_objects:
+            self.newbuildings.extend(page.newbuildings)
 
 
 class PageObject:
@@ -275,33 +261,94 @@ class PageObject:
         self.html_code = str()
         self.newbuildings = []
 
+    def update_newbuildings(self):
+        if self.newbuildings:
+            return
+        soup = BeautifulSoup(self.html_code, 'lxml')
+        url_blocks = soup.select('.Link__StyledLink-sc-1qa6dyr-0.jPkwaa.buildingCard__StyledAction-sc-1t8cw05-9.esqEyb')
+        new_buildings_urls = [HOST + quote(new_building['href']) for new_building in url_blocks]
+        self.newbuildings = [NewBuilding(self.city, url) for url in new_buildings_urls]
+
 
 class NewBuilding(Parser):
     def __init__(self, city, url):
         super().__init__()
         self.city = city
         self.url = url
+        self.name = unquote(self.url).split('/')[-1]
+        self.layout_page = LayoutPage(self, city, self.url + quote('/планировки'))
 
 
+class LayoutPage:
+    def __init__(self, newbuilding,  city,  url):
+        self.city = city
+        self.newbuilding = newbuilding
+        self.url = url
+        self.layouts = []
+
+    def update_layouts(self):
+        if self.layouts:
+            return
+
+
+class Layout(Parser):
+    def __init__(self, newbuilding, city, url):
+        super().__init__()
+        self.newbuilding = newbuilding
+        self.city = city
+        self.url = url
+        self.html_code = str()
+        self.updated_info = False
+        self.downloaded_images = False
+        self.image_source = str()
+        self.page_info = {}
+        self.name = str()
+        self.area = float()
+        self.price = float()
+
+    def update_info(self):
+        if self.updated_info:
+            return
+        response = self.request.get(self.url)
+        self.html_code = response.text
+        self.update_image_source()
+        self.update_page_info()
+        self.update_name()
+        self.update_area()
+        self.update_price()
+        self.updated_info = True
+
+    def update_image_source(self):
+        soup = BeautifulSoup(self.html_code, 'lxml')
+        image_block = soup.select_one('.SwipableGallery__StyledImage-q9ee6z-4')
+        self.image_source = 'https:' + image_block['src']
+
+    def update_page_info(self):
+        soup = BeautifulSoup(self.html_code, 'lxml')
+        self.page_info = {}
+        for element in soup.select('.KeyValue__StyledKeyValue-gwnrbl-0.bKluVn'):
+            self.page_info[element.select('div')[0].text] = element.select('div')[1].text
+
+    def update_name(self):
+        self.name = self.page_info['Планировка']
+
+    def update_area(self):
+        try:
+            self.area = float(self.page_info['Площадь'].replace(' м2', ''))
+        except ValueError:
+            self.area = None
+
+    def update_price(self):
+        soup = BeautifulSoup(self.html_code, 'lxml')
+        price = soup.select_one('.mainInfo__StyledPrice-sc-1k2gfo5-6.hIhsZO')
+        if price:
+            price_search = re.findall('\d+', price.text)
+            self.price = int(''.join(price_search))
 
 
 def main():
     parser = BuildingsParser()
-    parser.update_cities()
-    # parser = NewBuildingsData()
-    # cities = parser.get_cities_urls()
-    # cities.reverse()
-    # new_buildings_urls = parser.get_newbuildings_urls(cities)
-    # for city in new_buildings_urls:
-    #     parsed_cities = eval(load_file('cities'))
-    #     if city in parsed_cities:
-    #         continue
-    #     print(city)
-    #     layouts = parser.get_building_layouts(new_buildings_urls[city])
-    #     parser.save_layouts(layouts, city)
-    #     parsed_cities.append(city)
-    #     save_file(str(parsed_cities), 'cities')
-
+    parser.start()
 
 
 if __name__ == '__main__':
