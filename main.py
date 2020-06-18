63b9643cbfd096b90995402e4361592a7c3eddc5
isys35 <isys35@mail.ru>
2020-06-18 21:04:30 +0300

    debug

--- main.py
+++ main.py
@@ -389,8 +389,6 @@ class Layout(Parser):
 
 def main():
     parser = BuildingsParser()
-    #resp = parser.request.get('https://korter.ru/%D0%BD%D0%BE%D0%B2%D0%BE%D1%81%D1%82%D1%80%D0%BE%D0%B9%D0%BA%D0%B8-%D1%81%D0%BF%D0%B1')
-    #parser.save_html(resp.text, 'debug.html')
     parser.start()
 
 
